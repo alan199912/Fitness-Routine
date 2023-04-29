40ecade377d67a751df2dc2cca915b2c846ca9cb
Alan199912 <alan199912@gmail.com>
2023-04-29 17:10:57 -0300

    feat: profile

--- src/app/modules/dashboard/profile/profile.component.ts
+++ src/app/modules/dashboard/profile/profile.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '@app/core/models/user.models';
+import { UserService } from '@app/core/services/user/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  public user!: User;
 
+  constructor(private readonly userService: UserService) {}
+
+  public ngOnInit(): void {
+    this.getUser();
+  }
+
+  public getUser(): void {
+    this.userService.getUser().subscribe({
+      next: (user) => {
+        this.user = user;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
